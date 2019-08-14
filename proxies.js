0e30da87265f97c7993fca1978c40196cf3b5947
Randall Knutson <randall@knutson.io>
2019-08-14 14:17:50 -0500

    Add example of how to proxy authentication from bearer token to form.io token.

--- proxies.js
+++ proxies.js
@@ -1,8 +1,49 @@
+const request = require('request-promise-native');
+const jwt = require('jsonwebtoken');
+
 module.exports = {
   before: [
     (req, res, next) => {
-      console.log('Before proxy 1 called');
-      next();
+      // This is an example of changing out the authentication tokens for form.io tokens.
+      // You must know the JWT Secret set on the server to generate the token.
+
+      // Request the user from your server using the Authentication header or cookies.
+      request({
+        method: 'GET',
+        uri: 'https://reqres.in/api/users/2',
+        headers: {
+          'Authentication': req.headers.authentication
+        }
+      })
+        .then((response) => {
+          try {
+            const user = JSON.parse(response);
+
+            const token = jwt.sign({
+              external: true,
+              form: {
+                _id: 'USERFORMID' // Replace with the user resource id in your project
+              },
+              project: {
+                _id: 'PROJECTID' // Replace with the project id.
+              },
+              user: {
+                _id: user.data.id, // Can be any string.
+                data: user.data,
+                roles: [
+                  'ROLEIDTOGRANT' // Add the role ids to grant this user in the project.
+                ]
+              }
+            }, process.env.JWT_SECRET);
+
+            // Add Form.io jwt token to headers.
+            req.headers['x-jwt-token'] = token;
+            next();
+          }
+          catch (err) {
+            return next(err);
+          }
+        });
     },
     (req, res, next) => {
       console.log('Before proxy 2 called');
